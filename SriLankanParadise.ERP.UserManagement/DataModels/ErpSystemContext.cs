cea10c1d4d7620db047fc796119dd52fd18eebbb
AzureAD\AnuradhaMadhushani <ext_anuradha.madhushani@helloworld.com.au>
2023-11-02 07:58:17 +0530

    change db context file to use the connection string

--- SriLankanParadise.ERP.UserManagement/DataModels/ErpSystemContext.cs
+++ SriLankanParadise.ERP.UserManagement/DataModels/ErpSystemContext.cs
@@ -44,8 +44,7 @@ public partial class ErpSystemContext : DbContext
     public virtual DbSet<UserRole> UserRoles { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);Database=erp-system;User Id=sa;Password=local__pass@#;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:LocalSqlServerConnection");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
