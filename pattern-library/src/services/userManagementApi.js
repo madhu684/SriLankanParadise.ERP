491f5f037112f48f912f35f8919feb6b3d4a6ad8
Lakshitha <you@example.com>
2024-01-31 09:22:15 +0530

    Update environment configurations, package.json, and service files

--- pattern-library/src/services/userManagementApi.js
+++ pattern-library/src/services/userManagementApi.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://localhost:7287/api";
+//Import url from config file
+const baseUrl = process.env.REACT_APP_API_BASEURL;
+const baseUrl2 = process.env.REACT_APP_API_BASEURL2;
+const sublink = process.env.REACT_APP_API_SUBLINK;
+
+export const API_BASE_URL = `${baseUrl}${sublink}`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
