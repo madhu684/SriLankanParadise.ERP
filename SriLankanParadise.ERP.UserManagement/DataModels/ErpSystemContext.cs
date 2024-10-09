0abe51ca222b960559fc02ac525052039a74c05c
miyushan <miyushanrodrigo@gmail.com>
2024-10-09 16:03:52 +0530

    fix: update itemMaster models

--- SriLankanParadise.ERP.UserManagement/DataModels/ErpSystemContext.cs
+++ SriLankanParadise.ERP.UserManagement/DataModels/ErpSystemContext.cs
@@ -572,7 +572,7 @@ public partial class ErpSystemContext : DbContext
 
             entity.ToTable("ItemMaster");
 
-            entity.Property(e => e.ConversionRate).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.ConversionRate).HasColumnType("decimal(18, 6)");
             entity.Property(e => e.CreatedBy).HasMaxLength(50);
             entity.Property(e => e.ItemCode).HasMaxLength(50);
             entity.Property(e => e.ItemName).HasMaxLength(50);
