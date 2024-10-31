3ef3eb8b4c22c21b0cf101813ea30955611ec650
miyushan <miyushanrodrigo@gmail.com>
2024-10-31 17:47:32 +0530

    feat: complete inventory report related calculations

---
+++ SriLankanParadise.ERP.UserManagement/DataModels/Report.cs
@@ -0,0 +1,14 @@
+﻿namespace SriLankanParadise.ERP.UserManagement.DataModels
+{
+    public class Report
+    {
+        public string Inventory { get; set; }
+        public string RawMaterial { get; set; }
+        public string UOM { get; set; }
+        public double OpeningBalance { get; set; }
+        public double ReceivedQty { get; set; }
+        public string BatchNo { get; set; }
+        public double ActualUsage { get; set; }
+        public double ClosingBalance { get; set; }
+    }
+}
--- SriLankanParadise.ERP.UserManagement/ERP Web/Controllers/ReportController.cs
+++ SriLankanParadise.ERP.UserManagement/ERP Web/Controllers/ReportController.cs
@@ -18,20 +18,23 @@ namespace SriLankanParadise.ERP.UserManagement.ERP_Web.Controllers
         private readonly IDailyLocationInventoryService dailyLocationInventoryService;
         private readonly ILocationInventoryMovementService locationInventoryMovementService;
         private readonly ILocationInventoryService locationInventoryService;
+        private readonly IItemMasterService itemMasterService;
         private readonly IMapper mapper;
-        private readonly ILogger<DailyLocationInventory> logger;
+        private readonly ILogger<Report> logger;
 
         public ReportController(
             IDailyLocationInventoryService dailyLocationInventoryService,
             ILocationInventoryMovementService locationInventoryMovementService,
             ILocationInventoryService locationInventoryService,
+            IItemMasterService itemMasterService,
             IMapper mapper,
-            ILogger<DailyLocationInventory> logger
+            ILogger<Report> logger
         )
         {
             this.dailyLocationInventoryService = dailyLocationInventoryService;
             this.locationInventoryMovementService = locationInventoryMovementService;
             this.locationInventoryService = locationInventoryService;
+            this.itemMasterService = itemMasterService;
             this.mapper = mapper;
             this.logger = logger;
         }
@@ -47,10 +50,10 @@ namespace SriLankanParadise.ERP.UserManagement.ERP_Web.Controllers
                 var previousDateOnly = new DateOnly(previousDate.Year, previousDate.Month, previousDate.Day);
 
                 var alreadyStockedItemsLocInv = await dailyLocationInventoryService.Get(previousDateOnly, locationId);
-                var in_ItemsLocInv = await locationInventoryMovementService.ByDateRange(fromDate, toDate);
-                var out_ItemsLocInv = await locationInventoryMovementService.ByDateRange(fromDate, toDate);
+                var in_ItemsLocInv = await locationInventoryMovementService.ByDateRange(fromDate, toDate, 1);
+                var out_ItemsLocInv = await locationInventoryMovementService.ByDateRange(fromDate, toDate, 2);
 
-                // add already stocked items on selecting fromDate 
+                // add already stocked items on selecting fromDate (Inventory Up)
                 if (alreadyStockedItemsLocInv != null && alreadyStockedItemsLocInv.Any())
                 {
                     foreach (var item in alreadyStockedItemsLocInv)
@@ -58,29 +61,68 @@ namespace SriLankanParadise.ERP.UserManagement.ERP_Web.Controllers
                         inventoryItems.Add(new InventoryItemInfo
                         {
                             itemId = item.ItemMasterId,
+                            batchNumber = item.BatchNo,
                             ItemMaster = item.ItemMaster,
                             startingBalance = item.StockInHand ?? 0,
                             receivedQty = 0,
                             actualUsage = 0,
-                            closingBalance = item.StockInHand ?? 0
+                            closingBalance = 0
                         });
                     }
                 }
 
-                var locationInventoryMovements = await locationInventoryMovementService.ByDateRange(fromDate, toDate);
-                
-                if (locationInventoryMovements.Any())
+                // add incoming items (Inventory Up)
+                if (in_ItemsLocInv != null && in_ItemsLocInv.Any())
                 {
-                    var uniqueLocationInventoryMovements = locationInventoryMovements
-                        .GroupBy(l => new { l.ItemMasterId, l.BatchNo, l.LocationId })
-                        .Select(g => new
+                    foreach (var in_Item in in_ItemsLocInv)
+                    {
+                        // already stocked items
+                        if(inventoryItems.Any(i => i.itemId == in_Item.ItemMasterId && i.batchNumber == in_Item.BatchNo))
                         {
-                            g.Key.ItemMasterId,
-                            g.Key.BatchNo,
-                            g.Key.LocationId,
-                            ReceivedQty = g.Sum(l => l.Qty),
-                            ActualUsage = g.Sum(l => l.Qty)
-                        }).ToList();
+                            var index = inventoryItems.FindIndex(i => i.itemId == in_Item.ItemMasterId && i.batchNumber == in_Item.BatchNo);
+                            inventoryItems[index].receivedQty += in_Item.Qty ?? 0;
+                        }
+                        // new items
+                        else
+                        {
+                            var itemMaster = await itemMasterService.GetItemMasterByItemMasterId(in_Item.ItemMasterId);
+
+                            inventoryItems.Add(new InventoryItemInfo
+                            {
+                                itemId = in_Item.ItemMasterId,
+                                batchNumber = in_Item.BatchNo,
+                                ItemMaster = itemMaster,
+                                startingBalance = 0,
+                                receivedQty = in_Item.Qty ?? 0,
+                                actualUsage = 0,
+                                closingBalance = 0
+                            });
+                        }
+                    }
+                }
+
+                // add outgoing items (Inventory Down)
+                if (out_ItemsLocInv != null && out_ItemsLocInv.Any())
+                {
+                    foreach (var out_Item in out_ItemsLocInv)
+                    {
+                        if (inventoryItems.Any(i => i.itemId == out_Item.ItemMasterId && i.batchNumber == out_Item.BatchNo))
+                        {
+                            var index = inventoryItems.FindIndex(i => i.itemId == out_Item.ItemMasterId && i.batchNumber == out_Item.BatchNo);
+                            inventoryItems[index].actualUsage += out_Item.Qty ?? 0;
+                        }
+                    }
+                }
+
+                // calculate closing balance
+                foreach (var item in inventoryItems)
+                {
+                    item.closingBalance = item.startingBalance + item.receivedQty - item.actualUsage;
+                }
+
+                if (inventoryItems!=null && inventoryItems.Any())
+                {
+                   
 
                     
                     //var dailyLocationInventoryDto = mapper.Map<DailyLocationInventoryDto>(dailyLocationInventory);
@@ -98,6 +140,7 @@ namespace SriLankanParadise.ERP.UserManagement.ERP_Web.Controllers
         private class InventoryItemInfo
         {
             public int itemId { get; set; }
+            public string batchNumber { get; set; }
             public ItemMaster ItemMaster { get; set; }
             public decimal startingBalance { get; set; }
             public decimal receivedQty { get; set; }
