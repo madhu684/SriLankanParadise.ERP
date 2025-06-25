5aa5d58bcfc577e23c46d1c63b93a8b89151b305
SMaaliJay2000 <malithaj@novalabs.lk>
2025-06-25 14:51:39 +0530

    Change swagger environment

--- SriLankanParadise.ERP.UserManagement/Program.cs
+++ SriLankanParadise.ERP.UserManagement/Program.cs
@@ -323,11 +323,13 @@ var app = builder.Build();
 app.UseCors("AllowSpecificOrigin");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
