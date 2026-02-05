bd903cf416fd3c8d1f0b8ebb4957f6f843c99927
Maalitha Jayawardhana <malithaj@novalabs.lk>
2026-02-05 11:07:21 +0530

    Stock Report excel

--- SriLankanParadise.ERP.UserManagement/ERP Web/Controllers/ReportController.cs
+++ SriLankanParadise.ERP.UserManagement/ERP Web/Controllers/ReportController.cs
@@ -102,7 +102,8 @@ namespace SriLankanParadise.ERP.UserManagement.ERP_Web.Controllers
                             emptyReturnOut = 0,
                             emptyReturnReduce = 0,
                             adjustIn = 0,
-                            adjustOut = 0
+                            adjustOut = 0,
+                            trnIn = 0
                         });
                     }
                 }
@@ -147,7 +148,8 @@ namespace SriLankanParadise.ERP.UserManagement.ERP_Web.Controllers
                                 emptyReturnOut = 0,
                                 emptyReturnReduce = 0,
                                 adjustIn = 0,
-                                adjustOut = 0
+                                adjustOut = 0,
+                                trnIn = 0
                             };
                             inventoryItems.Add(itemDto);
                         }
@@ -172,6 +174,7 @@ namespace SriLankanParadise.ERP.UserManagement.ERP_Web.Controllers
                             case 13: itemDto.emptyReturnReduce += qty; break;
                             case 14: itemDto.adjustIn += qty; break;
                             case 15: itemDto.adjustOut += qty; break;
+                            case 18: itemDto.trnIn += qty; break;
                         }
 
                         // Calculate totalIn and totalOut based on MovementTypeId
